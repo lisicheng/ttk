c0bdb518e28c5f99852bfcc43ed514c93718c989
袁野 <yuanyelele@gmail.com>
2009-12-10 20:26:16 +0800

    update CSymTtkGc
    Signed-off-by: 袁野 <yuanyelele@gmail.com>

--- inc/symttk/gc.h
+++ inc/symttk/gc.h
@@ -6,11 +6,22 @@
 
 class CWsScreenDevice;
 
-class CSymTtkGc : public CWindowGc, public TtkGcInterface {
+class CSymTtkGc : public CBase, public TtkGcInterface {
+public:
+	virtual ~CSymTtkGc();
+	static CSymTtkGc* NewL(CWsScreenDevice& aScreen);
+	static CSymTtkGc* NewLC(CWsScreenDevice& aScreen);
 public: /* from TtkGcInterface */
 	void clear(const TtkRect& rect);
-private: /* from CWindowGc */
-	CSymTtkGc(CWsScreenDevice* aDevice);
+	void draw_line(const TtkPoint& p1, const TtkPoint& p2);
+	void set_clipping_rect(const TtkRect& rect);
+	void set_pen_color(TtkColor color);
+public:
+	CWindowGc& Gc() const;
+private:
+	void ConstructL(CWsScreenDevice& aScreen);
+private:
+	CWindowGc* iGc;
 };
 
 #endif // SYMTTK_GC_H
--- src/symttk/gc.cpp
+++ src/symttk/gc.cpp
@@ -2,13 +2,62 @@
 
 #include "ttk/common/rect.h"
 
-void CSymTtkGc::clear(const TtkRect& rect)
+CSymTtkGc::~CSymTtkGc()
 {
-	TRect sym_rect(rect.tl_.x_, rect.tl_.y_,
-		       rect.br_.x_, rect.br_.y_);
-	CWindowGc::Clear(sym_rect);
+	delete iGc;
 }
 
-CSymTtkGc::CSymTtkGc(CWsScreenDevice* aDevice) : CWindowGc(aDevice)
+CSymTtkGc* CSymTtkGc::NewL(CWsScreenDevice& aScreen)
 {
+	CSymTtkGc* self = CSymTtkGc::NewLC(aScreen);
+	CleanupStack::Pop(self);
+	return self;
+}
+
+CSymTtkGc* CSymTtkGc::NewLC(CWsScreenDevice& aScreen)
+{
+	CSymTtkGc* self = new(ELeave) CSymTtkGc();
+	CleanupStack::PushL(self);
+	self->ConstructL(aScreen);
+	return self;
+}
+
+void CSymTtkGc::clear(const TtkRect& rect)
+{
+	const TRect sym_rect(rect.tl_.x_, rect.tl_.y_,
+			     rect.br_.x_, rect.br_.y_);
+	iGc->Clear(sym_rect);
+}
+
+void CSymTtkGc::draw_line(const TtkPoint& p1, const TtkPoint& p2)
+{
+	const TPoint sym_p1(p1.x_, p1.y_);
+	const TPoint sym_p2(p2.x_, p2.y_);
+	iGc->DrawLine(sym_p1, sym_p2);
+}
+
+void CSymTtkGc::set_clipping_rect(const TtkRect& rect)
+{
+	const TRect sym_rect(rect.tl_.x_, rect.tl_.y_,
+			     rect.br_.x_, rect.br_.y_);
+	iGc->SetClippingRect(sym_rect);
+}
+
+void CSymTtkGc::set_pen_color(TtkColor color)
+{
+	const TRgb sym_color(color & kTtkColorRawRed,
+			     color & kTtkColorRawGreen,
+			     color & kTtkColorRawBlue,
+			     color & kTtkColorRawAlpha);
+	iGc->SetPenColor(sym_color);
+}
+
+CWindowGc& CSymTtkGc::Gc() const
+{
+	return *iGc;
+}
+
+void CSymTtkGc::ConstructL(CWsScreenDevice& aScreen)
+{
+	User::LeaveIfError(aScreen.CreateContext(iGc));
 }
