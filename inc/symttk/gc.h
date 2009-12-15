f8c06aa58aeb90b36fbd72714f4326cef694b18b
袁野 <yuanyelele@gmail.com>
2009-12-15 10:56:56 +0800

    add draw_text support
    Signed-off-by: 袁野 <yuanyelele@gmail.com>

--- inc/symttk/gc.h
+++ inc/symttk/gc.h
@@ -15,15 +15,19 @@ public: /* from TtkGcInterface */
 	void clear(const TtkRect& rect);
 	void draw_line(const TtkPoint& p1, const TtkPoint& p2);
 	void draw_rect(const TtkRect& rect);
+	void draw_text(const unsigned char* text, const TtkRect& rect);
 	void set_clipping_rect(const TtkRect& rect);
 	void set_pen_color(TtkColor color);
 	void set_brush_color(TtkColor color);
 public:
 	CWindowGc& Gc() const;
 private:
-	void ConstructL(CWsScreenDevice& aScreen);
+	CSymTtkGc(CWsScreenDevice& aScreen);
+	void ConstructL();
 private:
 	CWindowGc* iGc;
+	CWsScreenDevice& iScreen;
+	CFont* iFont;
 };
 
 #endif /* SYMTTK_GC_H */
