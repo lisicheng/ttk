133568e6be5d16d828204f19441fb1294fd93a82
袁野 <yuanyelele@gmail.com>
2010-01-05 18:08:38 +0800

    intersection
    Signed-off-by: 袁野 <yuanyelele@gmail.com>

--- src/ttk/common/rect.cpp
+++ src/ttk/common/rect.cpp
@@ -32,3 +32,25 @@ int TtkRect::height() const
 {
 	return br_.y_ - tl_.y_;
 }
+
+void TtkRect::intersection(const TtkRect& rect)
+{
+	if (tl_.x_ < rect.tl_.x_) tl_.x_ = rect.tl_.x_;
+	if (br_.x_ > rect.br_.x_) br_.x_ = rect.br_.x_;
+	if (tl_.y_ < rect.tl_.y_) tl_.y_ = rect.tl_.y_;
+	if (br_.y_ > rect.br_.y_) br_.y_ = rect.br_.y_;
+	if (!intersects(rect)) {
+		br_.x_ = tl_.x_;
+		br_.y_ = tl_.y_;
+	}
+}
+
+bool TtkRect::intersects(const TtkRect& rect) const
+{
+	if (tl_.x_ < rect.br_.x_ && rect.tl_.x_ < br_.x_ &&
+	    tl_.y_ < rect.br_.y_ && rect.tl_.y_ < br_.y_)
+		return true;
+	else
+		return false;
+}
+
